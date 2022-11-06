c4012b338dffaf7e5678c58518d3d738ee51f91a
Tomasz Borowski <bortom112233@gmail.com>
2022-11-06 13:31:28 +0100

    init api

---
+++ src/test/java/tb/pl/todoapi/ToDoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tb.pl.todoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
