a1f3c48135e3c52ffdc8463dc0028f485a0087af
Tomasz Borowski <bortom112233@gmail.com>
2022-11-09 15:39:34 +0100

    hardcode backend to welcome new todos to save

--- src/main/java/tb/pl/todoapi/ToDoHardcodedService.java
+++ src/main/java/tb/pl/todoapi/ToDoHardcodedService.java
@@ -13,14 +13,30 @@ public class ToDoHardcodedService {
     private static int counter = 0;
 
     static {
-        todos.add(new ToDo(counter++, "In28Minutes", "Learn to dance", new Date(), true));
-        todos.add(new ToDo(counter++, "Travel-travel", "Visit Spain", new Date(), true));
-        todos.add(new ToDo(counter++, "Programmer23", "Exercise React", new Date(), true));
+        todos.add(new ToDo(counter++, "In28Minutes", "Learn to dance", new Date(), false));
+        todos.add(new ToDo(counter++, "Travel-travel", "Visit Spain", new Date(), false));
+        todos.add(new ToDo(counter++, "Programmer23", "Exercise React", new Date(), false));
     }
 
     public List<ToDo> findAllTodos() {
         return todos;
     }
+
+    public ToDo save(ToDo todo) {
+        if (todo.getId() == -1) {
+            todo.setId(++counter);
+            todos.add(todo);
+        } else {
+            deleteById(todo.getId());
+            todos.add(todo);
+        }
+        return todo;
+    }
+
+    private void deleteById(Integer id) {
+        todos.removeIf(todo1 -> todo1.getId() == id);
+    }
+
 
     Optional<ToDo> removeItem(Integer id) {
         Optional<ToDo> todoToRemove = findById(id);
@@ -31,7 +47,7 @@ public class ToDoHardcodedService {
         return todoToRemove;
     }
 
-    private Optional<ToDo> findById(Integer id) {
+    public Optional<ToDo> findById(Integer id) {
         return todos.stream().filter(todo -> todo.getId() == id).findFirst();
     }
 }
--- src/main/java/tb/pl/todoapi/ToDoResource.java
+++ src/main/java/tb/pl/todoapi/ToDoResource.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.ArrayDeque;
 import java.util.List;
 import java.util.Optional;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
@@ -20,6 +25,11 @@ public class ToDoResource {
         return toDoHardcodedService.findAllTodos();
     }
 
+    @GetMapping("/users/{username}/todos/{id}")
+    public ToDo getToDo(@PathVariable String username, @PathVariable Integer id) {
+        return toDoHardcodedService.findById(id).orElseThrow(() -> new RuntimeException("nothing found by this id"));
+    }
+
     @DeleteMapping("/users/{username}/todos/{id}")
     public ResponseEntity<Void> removeToDo(@PathVariable String username,@PathVariable Integer id) {
         Optional<ToDo> toDoToRemove = toDoHardcodedService.removeItem(id);
@@ -28,5 +38,18 @@ public class ToDoResource {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+    @PutMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<ToDo> updateToDo(@PathVariable String username,@PathVariable Integer id, @RequestBody ToDo todo) {
+        System.out.println("in put method");
+        ToDo saved = toDoHardcodedService.save(todo);
+        return new ResponseEntity<>(saved, HttpStatus.OK);
+    }
+
+    @PostMapping("/users/{username}/todos")
+    public ResponseEntity<String> updateToDo(@PathVariable String username, @RequestBody ToDo todo) {
+        ToDo saved = toDoHardcodedService.save(todo);
+        String uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(saved.getId()).toUri().toString();
+        return new ResponseEntity<>(uri, HttpStatus.CREATED);
     }
 }
